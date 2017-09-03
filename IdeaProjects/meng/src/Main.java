7c66f5e0a838ca07b21948b49b8ed89c3e941f92
Meng <pmtea@qq.com>
2017-09-03 16:52:55 +0800

    方法调用

--- IdeaProjects/meng/src/Main.java
+++ IdeaProjects/meng/src/Main.java
@@ -1,12 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        int i = 0;
-        for (; i<3; i++){
-           System.out.println('*');
+        //如何调用方法，不调用不执行
+
+        //单独调用。不推荐，没有意义
+        //输出调用，不够好，因为可能还会需要使用
+        //赋值调用，推荐
+        int a =50;
+        int b=60;
+        int c = sum(a,b);
+
+        c=c+1;
+        c++;
+        System.out.println(c);
         }
-    }
+
+
+    public static int sum(int a,int b){
+        int c =a+b;
+        return c;
+        }
+
+
 
 }
